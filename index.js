fc48090a28f9fbd2ea9b7bc131f43c4a844b896d
wnm <beter.90@mail.ru>
2019-03-24 22:30:57 +0600

    first steps
    Hey-low-hello, world :boat:

---
+++ index.js
@@ -0,0 +1,151 @@
+const fetch = require('node-fetch')
+const Discord = require(`discord.js`);
+const Jimp = require(`Jimp`);
+const mysql = require('mysql');
+const client = new Discord.Client();
+var cooldowns = {};
+var con = mysql.createConnection({
+    host: process.env.host,
+    user: process.env.user,
+    password: process.env.pass,
+    database: process.env.database
+});
+var guilds_settings = {};
+
+setInterval(() => {
+    for (var i = 0; i < Object.keys(cooldowns).length; i++) {
+        cooldowns[Object.keys(cooldowns)[i]] -= 1;
+        if (cooldowns[Object.keys(cooldowns)[i]] == 0) {
+            delete cooldowns[Object.keys(cooldowns)[i]]
+        }
+    }
+}, 1000);
+
+con.connect(function (err) {
+    if (err) return console.log(err);
+    console.log('MYSQL is READY!')
+    client.on('ready', () => {
+        console.log('CLIENT is READY!');
+        client.user.setActivity('+setchannel', {
+            type: 'WATCHING'
+        });
+    })
+    setInterval(() => {
+        con.query(`SELECT 1`)
+    }, 20000)
+    con.query("SELECT * FROM `guilds`", (err, data) => {
+        if (err) throw err;
+        data.map(e => {
+            guilds_settings[e.guild_id] = e.workchannel;
+        })
+    })
+
+    client.on('guildCreate', guild => {
+        guild.channels.find(ch => ch.position == 0).send({
+            embed: {
+                title: `Hi guys :D`,
+                description: "To set work channnel: **+setchannel #chat_name**\n" + "example: +setchannel " + guild.channels.find(ch => ch.position == guild.channels.filter(chan => chan.type == 'text').length - 1) + "\nIf you wonna add me to another server: [Click here](https://discordapp.com/api/oauth2/authorize?client_id=559247918280867848&permissions=0&scope=bot)",
+                footer: {
+                    icon_url: client.user.displayAvatarURL,
+                    text: `Ony admins can use that command!`
+                },
+            }
+        })
+    })
+
+    client.on('message', msg => {
+        if (msg.content.toLowerCase().startsWith('+setchannel')) {
+            if (msg.member.hasPermission('KICK_MEMBERS')) {
+                if (cooldowns[msg.guild.id]) return msg.channel.send(`This command is on cooldown on this server. Please wait **${cooldowns[msg.guild.id]} sec.**`)
+                cooldowns[msg.guild.id] = 20;
+                console.log(cooldowns)
+                var chn = msg.content.split(' ')[1]
+                if (!chn) return msg.channel.send({
+                    embed: {
+                        color: 0xff0000,
+                        description: `:interrobang: Syntax: **+setchannel ${msg.guild.channels.find(ch => ch.position == 1 || ch.type == "text")}**`
+                    }
+                })
+                chn = chn.replace(/</gi, '').replace(/>/gi, '').replace(/#/gi, '');
+                if (!chn || chn.length !== 18 || msg.guild.channels.find(ch => ch.id == chn) == null) return msg.channel.send({
+                    embed: {
+                        color: 0xff0000,
+                        description: `:interrobang: Syntax: **+setchannel ${msg.guild.channels.find(ch => ch.position == 1 || ch.type == "text")}**`
+                    }
+                })
+                if (guilds_settings[msg.guild.id]) {
+                    con.query("UPDATE `guilds` SET workchannel =" + chn, (err) => {
+                        if (err) return msg.react('⁉')
+                        guilds_settings[msg.guild.id] = chn;
+                    });
+                } else {
+                    con.query("INSERT INTO `guilds` VALUES ('" + msg.guild.id + "','" + chn + "')", (err) => {
+                        if (err) return msg.react('⁉')
+                        guilds_settings[msg.guild.id] = chn;
+                    });
+                }
+                msg.react('👌')
+                msg.guild.channels.find(ch => ch.id == chn).send('__**<:animePodsos:559025418221518850>ANIME-HUNTER IS HERE, GIMME UR ANIME SCREENSHOTS<:animePodsos:559025418221518850>**__ ')
+            } else {
+                msg.channel.send(`You don't have enough permissions.`)
+            }
+        }
+        if (msg.channel.id !== guilds_settings[msg.guild.id]) return
+        var attch = msg.attachments.first()
+        if (attch) {
+            var url = attch.url;
+            var urlToArr = url.toLowerCase().split('.')
+            if (['png', 'jpeg', 'jpg'].indexOf(urlToArr[urlToArr.length - 1]) == -1) return
+            Jimp.read(url, function (err, img) {
+                if (err) throw err;
+                img.resize(720, 480).getBase64(Jimp.AUTO, function (e, img64) {
+                    if (e) throw e
+                    fetch('https://trace.moe/api/search', {
+                            method: 'POST',
+                            body: JSON.stringify({
+                                image: img64
+                            }),
+                            headers: {
+                                'Content-Type': 'application/json'
+                            }
+                        })
+                        .then(res => {
+                            return res.json();
+                        })
+                        .then(result => {
+                            var unique = function (arr) {
+                                let newarr = [];
+                                arr.map(e => {
+                                    if (newarr.indexOf(e) == -1) newarr = [...newarr, e]
+                                })
+                                return newarr
+                            }
+                            var rawdesc = result.docs.map((e, i) => {
+                                return `[${e.title_romaji}](https://anilist.co/anime/${e.anilist_id})`
+                            });
+                            rawdesc = unique(rawdesc);
+
+                            msg.channel.send({
+                                embed: {
+                                    title: `There's some results:`,
+                                    description: rawdesc.join(`\n`),
+                                    color: 7589871,
+                                    thumbnail: {
+                                        url: url,
+                                    },
+                                    footer: {
+                                        icon_url: msg.author.displayAvatarURL,
+                                        text: `requested by ${msg.author.username}, author: wnm#1663`
+                                    },
+                                }
+                            })
+                        });
+                })
+
+            })
+
+
+        }
+    })
+});
+client.login(process.env.TOKEN)
