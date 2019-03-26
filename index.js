e42d50aaf51ebd9b1f0537621a5b5a23515cf1bb
wnm <31887794+leso1337@users.noreply.github.com>
2019-03-26 10:56:24 +0600

    Update index.js

--- index.js
+++ index.js
@@ -95,8 +95,8 @@ con.connect(function (err) {
         const img_formats = ['png', 'jpeg', 'jpg'];
         var attch = msg.attachments.first()
                 if(msg.content.indexOf(".")> -1 && 
-           msg.content.startwith('http') &&
-           img_formats.indexOf(msg.content.split('.')[msg.content.split('.').length - 1]) == -1) {
+                   msg.content.startsWith('http') &&
+                   img_formats.indexOf(msg.content.split('.')[msg.content.split('.').length - 1]) == -1) {
              attch = msg.content;
         }
         if (attch) {
